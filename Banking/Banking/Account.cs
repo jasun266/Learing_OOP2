e3fa8308c51a21ddfed2b21350b9ead7ce5592b0
JASUN <jasun266@gmail.com>
2021-03-01 00:51:12 +0600

    account class  with all information

--- Banking/Banking/Account.cs
+++ Banking/Banking/Account.cs
@@ -6,40 +6,104 @@ using System.Threading.Tasks;
 
 namespace BankingSystem
 {
+	struct Address 
+	{
+		private String readNo;
+		private String houseNo;
+		private String city;
+		private String country;
+
+		public Address(String readNo, string houseNo, string city, String country)
+		{
+			this.readNo		= readNo;
+			this.houseNo	= houseNo;
+			this.city		= city;
+			this.country	= country;
+
+		}
+		public string GetAddress ()
+		{
+			return this.readNo+" "+this.houseNo+" "+this.city+ " "+this.country;
+		}
+	}
 	class Account
 	{
 
 		private int accountNumber;
 		private string accountName;
 		private double balance;
-		//private Address address;
+		private Address address;
 
 		public Account(string accountName, double balance, Address address)
 		{
-			//this.accountNumber // auto generate
-			this.accountName = accountName;
-			this.balance = balance;
-			this.address = address;
+			this.accountNumber	 = accountNumber; // auto generate
+			this.accountName	 = accountName;
+			this.balance		 = balance;
+			this.address		 = address;
+		}
+
+		public int AccountNumber 
+		{
+			set { this.accountNumber = value; }
+			get { return this.accountNumber; }
+		}
+
+		public string AccountName 
+		{
+			set { this.accountName = value; }
+			get { return this.accountName; }
+		}
+		public double Balance 
+		{
+			set { this.balance = value; }
+			get { return this.balance; }
+		}
+		public Address Address 
+		{
+			set { this.address = value; }
+			get { return this.address; }
 		}
 
 		public void Withdraw(double amount)
 		{
-
+			if (this.balance - amount >500 )
+			{
+				this.balance = this.balance - amount;
+				Console.WriteLine("{0} wihdraw successful", amount);
+			}
+			else 
+			{
+				Console.WriteLine("insufficient account balance....");
+			}
 		}
 
 		public void Deposit(double amount)
 		{
-
+			this.balance = this.balance + amount;
+			Console.WriteLine("{0} amount has been added", amount);
 		}
 
 		public void Transfer(int receiver, double amount)
 		{
-
+			if(this.balance - amount > 500 && this.accountNumber == receiver) 
+			{
+				this.balance = this.balance - amount;
+				Console.WriteLine("{0} TK has been transfer", amount);
+			}
 		}
 
-		public void ShowAccountInformation(double amount)
+		public void ShowAccountInformation()
 		{
-
+			Console.WriteLine("Account ID               --> {0}", this.accountNumber);
+            Console.WriteLine("Account holder's Name    --> {0}", this.accountName);
+            Console.WriteLine();
+            Console.WriteLine("Owner's address ");
+           // this.Address();
+            Console.WriteLine();
+            Console.WriteLine("Current account balance  --> {0}", this.Balance);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("---  ---  ---  ---  ---  ---  ---  ---  ---  ---  ---  ");
 		}
 
 	}
